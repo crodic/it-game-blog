f4fabb6c33bfb0647840266d1b759fddf4e5b4eb
crodic <alice01422@gmail.com>
2025-02-09 09:39:20 +0700

    Update CheckServerStatus to disable caching for API settings request

--- src/components/check-server-status.tsx
+++ src/components/check-server-status.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation';
 import xior from 'xior';
 
 export default async function CheckServerStatus() {
-    await xior.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/settings`).catch((error) => {
+    await xior.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/settings`, { cache: 'no-store' }).catch((error) => {
         if (error.response?.status === 502 || error.response?.status === 503) {
             notFound();
         }
