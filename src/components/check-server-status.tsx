c16265e5ed3e73e331c7d96287c368db3573454e
crodic <alice01422@gmail.com>
2025-02-09 09:33:55 +0700

    Fix error handling in CheckServerStatus component to safely access response status

--- src/components/check-server-status.tsx
+++ src/components/check-server-status.tsx
@@ -2,8 +2,8 @@ import { notFound } from 'next/navigation';
 import xior from 'xior';
 
 export default async function CheckServerStatus() {
-    await xior.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/settings`, { cache: 'no-store' }).catch((error) => {
-        if (error.response.status === 502 || error.response.status === 503) {
+    await xior.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/settings`).catch((error) => {
+        if (error.response?.status === 502 || error.response?.status === 503) {
             notFound();
         }
     });
